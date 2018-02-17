11e3c37a08d57cdb0411b7b688d6b2d7bf9feb1a
Pierre Marchand <pierre.jimthefrench@gmail.com>
2018-02-17 16:13:26 +0100

    Unecessary barrier removed

--- tests/functional_tests/tests_solve/tests_ddm_HE_2D_P1_indirect_hmat.cpp
+++ tests/functional_tests/tests_solve/tests_ddm_HE_2D_P1_indirect_hmat.cpp
@@ -53,7 +53,6 @@ int main(int argc, char *argv[]) {
     Orienting(mesh);
     mesh = unbounded;
     int nb_elt = NbElt(mesh);
-    MPI_Barrier(MPI_COMM_WORLD);
 
     // Mesh
     Geometry node_output(meshname_output);
